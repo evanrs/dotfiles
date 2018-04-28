1cf6aaebf1186b35596ef6e4fe65ef44ca9f2220
Evan Schneider <me@evanrs.com>
2018-04-28 08:04:30 -0400

    Remove prezto as submodule

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env zsh
+
+if ! [ -e "${ZDOTDIR:-$HOME}/.zprezto" ]
+then
+  echo "Cloning prezto into  ${ZDOTDIR:-$HOME}/.zprezto"
+  git clone --recursive https://github.com/evanrs/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+fi
+
 setopt EXTENDED_GLOB
-
-ln -sFfv "${ZDOTDIR:-$HOME}"/.dotfiles/zsh/prezto "${ZDOTDIR:-$HOME}"/.zprezto
-
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^(zshrc|README.md)(.N); do
   ln -sfv "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
-chsh -s /bin/zsh
+ln -sfv "$ZSH/zsh/zshrc.symlink" "${ZDOTDIR:-$HOME}/.zshrc"
+
+if [ $SHELL != '/bin/zsh' ]
+then
+  echo "Setting /bin/zsh as default shell"
+  chsh -s /bin/zsh
+fi
---
+++ zsh/prezto.zsh
@@ -0,0 +1,11 @@
+source "${ZDOTDIR:-$HOME}/.zprezto/runcoms/zshrc"
+
+##
+## Here be dragons
+##
+## (∩ ͡° ͜ʖ ͡°)⊃━☆ﾟ. *
+##
+
+autoload -Uz promptinit
+promptinit
+prompt pure
