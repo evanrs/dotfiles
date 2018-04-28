c315661237f13c01691eb0d0bfd48712f7274734
Evan Schneider <me@evanrs.com>
2018-04-28 08:10:35 -0400

    Update iterm2 install

--- iterm2/install.sh
+++ iterm2/install.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -e
 
-defaults write com.googlecode.iterm2 PrefsCustomFolder -string "~/.dotfiles/iterm2"
+defaults write com.googlecode.iterm2 PrefsCustomFolder -string "${ZROOTDIR:-$HOME}/.dotfiles/iterm2"
 defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -int 1
 
-echo "Linking ~/.dotfiles/iterm2/profiles to ~/Library/Application Support/iTerm2/DynamicProfiles"
+echo "Linking ${ZROOTDIR:-$HOME}/.dotfiles/iterm2/profiles to ${ZROOTDIR:-$HOME}/Library/Application Support/iTerm2/DynamicProfiles"
 
-mkdir -p "~/Library/Application Support/iTerm2/"
-rm -rf "~/Library/Application Support/iTerm2/DynamicProfiles"
+mkdir -p "${ZROOTDIR:-$HOME}/Library/Application Support/iTerm2/"
+rm -rf "${ZROOTDIR:-$HOME}/Library/Application Support/iTerm2/DynamicProfiles"
 
-ln -sfF ~/.dotfiles/iterm2/profiles ~/Library/Application\ Support/iTerm2/DynamicProfiles
+# ln -sfF "${ZROOTDIR:-$HOME}/.dotfiles/iterm2/profiles" "${ZROOTDIR:-$HOME}/Library/Application\ Support/iTerm2/DynamicProfiles"
