81dc019309a0592125280cbbb91e3c6c2328ef09
Evan Schneider <me@evanrs.com>
2018-04-27 20:41:56 -0400

    Scrub those unwanted services from macOS

--- macos/clean.sh
+++ macos/clean.sh
@@ -1,4 +1,30 @@
 #!/bin/sh
 
-# fuck citrix
-sudo launchctl remove com.citrix.ServiceRecords
+# List of specific services
+services=(
+	# com.company.nuisance
+)
+
+# List of targets to match
+targets=(
+	adobe
+	amazon.sendtokindle
+	dropbox
+	citrix
+	google.keystone
+	GoToMeeting
+	oracle.java
+	skype
+	valvesoftware.steamclean
+)
+
+
+for target in ${targets[@]} ; do
+	services+=(`launchctl list | grep "\.$target\." | awk '{print $3}'`) ;
+done
+
+# for service in ${services[@]} ; do echo "${service}" ; done
+for service in ${services[@]} ; do
+	echo Removing $service ;
+	launchctl remove ${service} ;
+done
