beb1356e9932905215a4e6a657a4c6294b35fc6c
Evan Schneider <me@evanrs.com>
2018-04-29 11:02:07 -0400

    Update macos/clean.sh

--- macos/clean.sh
+++ macos/clean.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-# List of specific services
-services=(
-	# com.company.nuisance
-)
-
 # List of targets to match
 targets=(
 	adobe
@@ -18,13 +13,40 @@ targets=(
 	valvesoftware.steamclean
 )
 
+# Paths to match targets from
+paths=(
+    "/Library/LaunchAgents"
+    "$HOME/Library/LaunchAgents"
+)
 
+# List of specific services to remove
+services=(
+    # com.company.nuisance
+)
+
+# List of specific files to remove
+files=(
+    # /Library/LaunchAgents/nuisance
+)
+
+# Accumulate services and files for removal and unloading
 for target in ${targets[@]} ; do
 	services+=(`launchctl list | grep "\.$target\." | awk '{print $3}'`) ;
+
+    for path in ${paths[@]}; do
+        path_files=(`ls -a1 "$path" | grep "\.$target\."`)
+        files+=("${path_files[@]/#/$path/}")
+    done
 done
 
-# for service in ${services[@]} ; do echo "${service}" ; done
-for service in ${services[@]} ; do
-	echo Removing $service ;
-	launchctl remove ${service} ;
+# Remove services
+for service in ${services[@]}; do
+	echo Removing $service
+    launchctl remove ${service} ;
+done
+
+# Unload files
+for file in ${files[@]}; do
+    echo Unloading $file
+    launchctl unload ${file} ;
 done
