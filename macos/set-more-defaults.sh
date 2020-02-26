fd4aa0c513bea9829a5b3a9add2a7446dad1a3c3
evanrs <me@evanrs.com>
2020-02-25 21:32:38 -0500

    feat: add iphone simulator option

--- macos/set-more-defaults.sh
+++ macos/set-more-defaults.sh
@@ -1,2 +1,5 @@
 # Set apps to show function keys
 defaults write com.apple.touchbar.agent PresentationModePerApp -dict-add "com.citrix.XenAppViewer" functionKeys
+
+# Allow simulator in full screen
+defaults write com.apple.iphonesimulator AllowFullscreenMode -bool YES
