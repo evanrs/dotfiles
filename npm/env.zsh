45dda2f83243b15284923b3a41824135c98bf875
evanrs <me@evanrs.com>
2021-05-21 17:02:08 -0400

    fix: hack pnpm global bin onto path

--- npm/env.zsh
+++ npm/env.zsh
@@ -1,3 +1,7 @@
+# TODO a less fragile link to global modules when available …
+export PNPM_GLOBAL_BIN=`dirname \`volta which node\``
+export PATH="$PNPM_GLOBAL_BIN:$PATH"
+
 function npm-preview() {
   npm pack && tar -xvzf *.tgz && rm -rf package *.tgz
 }
