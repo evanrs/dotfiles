caf2c926bd0cb0b094f4e1096f344f737a2e743c
evanrs <me@evanrs.com>
2021-12-23 22:20:45 -0500

    refactor: use volta

--- node/install.sh
+++ node/install.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-# --skip-setup defers to properties set in node/env.zsh
-curl https://get.volta.sh | bash -s -- --skip-setup
-
-volta install node
+# set default node version
+volta install node@14
---
+++ node/pre-install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# --skip-setup defers to properties set in node/env.zsh
+curl https://get.volta.sh | bash -s -- --skip-setup
+
+# set default node version
+volta install node@14
+
+# remove corepack assets
+npm uninstall -gf corepack &> /dev/null
+bin=$(dirname $(which npm))
+rm -rf "$bin/../lib/node_modules/corepack"
+for executable in yarn pnpm pnpx
+do
+   rm -f "$bin/$executable" &> /dev/null
+done
+
+# use corepack to integrate yarn and pnpm
+npm install -g corepack
+
+# install dotfiles node dependencies
+pnpm --prefix ~/.dotfiles install
